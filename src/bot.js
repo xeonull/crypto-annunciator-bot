01181660f68293ca2773b766ef3883e0f8bbd321
xeonull <xeonull@gmail.com>
2022-04-05 22:26:19 +0300

    #1

--- src/bot.js
+++ src/bot.js
@@ -5,7 +5,18 @@ nconf.argv().env().file({ file: 'config.json' })
 
 const bot = new Bot(nconf.get('telegramApiKey'))
 
-bot.command('start', (ctx) =>
+bot.api.setMyCommands([
+  {
+    command: 'start',
+    description: 'Start',
+  },
+  {
+    command: 'ticker',
+    description: 'set ticker',
+  },
+])
+
+bot.command('start', (ctx) => {
   ctx.reply('Welcome to the Crypto Annunciator Bot', {
     reply_markup: {
       inline_keyboard: [
@@ -17,15 +28,37 @@ bot.command('start', (ctx) =>
           {
             text: 'Limit',
             callback_data: 'set_ticker',
-          }
+          },
         ],
       ],
     },
   })
+})
+
+bot.command('ticker', (ctx) =>
+  ctx.reply('---', {
+    text: 'Please give us your phone number',
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [
+          {
+            text: 'Share my phone number',
+            request_contact: true,
+          },
+        ],
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    }),
+  })
 )
 
-bot.command('ticker')
-
-bot.on('message', (ctx) => ctx.reply('Got another message!'))
+bot.on('message', (ctx) =>
+  ctx.api.sendMessage(ctx.message.chat.id, 'Some text...', {
+    reply_markup: JSON.stringify({
+      hide_keyboard: true,
+    }),
+  })
+)
 
 bot.start()
